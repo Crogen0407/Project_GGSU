039423b279631b1ab56db4ae33d9fcc1bef29e5a
Crogen0407 <mymcoding0407@gmail.com>
2025-08-26 11:34:28 +0900

    feat : Building Class...

--- Source/Project_GGSU/DateSystem/GGSUDateController.cpp
+++ Source/Project_GGSU/DateSystem/GGSUDateController.cpp
@@ -1,11 +1,13 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "DateController.h"
+#include "GGSUDateController.h"
 #include "Components/DirectionalLightComponent.h"
 
+float AGGSUDateController::Time = 0.0f;
+
 // Sets default values
-ADateController::ADateController()
+AGGSUDateController::AGGSUDateController()
 {
 	PrimaryActorTick.bCanEverTick = true;
 
@@ -15,17 +17,17 @@ ADateController::ADateController()
 }
 
 // Called when the game starts or when spawned
-void ADateController::BeginPlay()
+void AGGSUDateController::BeginPlay()
 {
 	Super::BeginPlay();
 	
 }
 
 // Called every frame
-void ADateController::Tick(float DeltaTime)
+void AGGSUDateController::Tick(float DeltaTime)
 {	
 	Super::Tick(DeltaTime);
-	Time += DeltaTime * 10;
-	DirectionalLightComponent->SetWorldRotation(FRotator(Time+90, 0, 0));
+	AGGSUDateController::Time += DeltaTime * 10;
+	DirectionalLightComponent->SetWorldRotation(FRotator(AGGSUDateController::Time+90, 0, 0));
 }
 
