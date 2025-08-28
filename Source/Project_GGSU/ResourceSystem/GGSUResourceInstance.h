98cbb1f33ae85203ddae1bbb655544b7e5e49193
dongdong5899 <dong012673@gmail.com>
2025-08-28 15:06:23 +0900

    feat: ResourceInstance

---
+++ Source/Project_GGSU/ResourceSystem/GGSUResourceInstance.h
@@ -0,0 +1,46 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "GGSUResourceInstance.generated.h"
+
+UENUM()
+enum EResourceType
+{
+	None = 0,
+	Gold,
+	
+};
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT_GGSU_API UGGSUResourceInstance : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+
+
+public:
+	uint32 GetResource(EResourceType type) { return ResourceAmount[type]; }
+	void AddResource(EResourceType type, uint32 value) { ResourceAmount[type] += value; }
+	void RemoveResource(EResourceType type, uint32 value)
+	{
+		if (ResourceAmount[type] >= value)
+			ResourceAmount[type] -= value;
+	}
+	bool TryRemoveResource(EResourceType type, uint32 value)
+	{
+		if (ResourceAmount[type] >= value)
+		{
+			ResourceAmount[type] -= value;
+			return true;
+		}
+		return false;
+	}
+	
+private:
+	TMap<EResourceType, uint32> ResourceAmount;
+};
