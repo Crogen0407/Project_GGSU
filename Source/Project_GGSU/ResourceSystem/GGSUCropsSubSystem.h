4aa08e202f1d8510bd5f527bc10bbe178a6e47a9
Crogen0407 <mymcoding0407@gmail.com>
2025-09-04 00:39:56 +0900

    feat : Title Level Batch

---
+++ Source/Project_GGSU/ResourceSystem/GGSUCropsSubSystem.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "GGSUCropsSubSystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT_GGSU_API UGGSUCropsSubSystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+	
+public:
+};
