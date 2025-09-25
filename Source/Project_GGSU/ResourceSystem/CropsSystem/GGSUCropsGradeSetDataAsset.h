798183e81b9eddb2b18dde90c83d4a970655e260
Crogen0407 <mymcoding0407@gmail.com>
2025-09-25 19:59:35 +0900

    feat : Crops set data asset

--- Source/Project_GGSU/ResourceSystem/CropsSystem/GGSUCropsGradeSetDataAsset.h
+++ Source/Project_GGSU/ResourceSystem/CropsSystem/GGSUCropsGradeSetDataAsset.h
@@ -1,22 +1,21 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
-#include "ResourceSystem/GGSUResourceDataAsset.h"
+#include "GGSUCropDataAsset.h"
 #include "GGSUCropsGradeSetDataAsset.generated.h"
 
-class UGGSUCropDataAsset;
-/**
- * 
- */
 UCLASS()
-class PROJECT_GGSU_API UGGSUCropsGradeSetDataAsset : public UGGSUResourceDataAsset
+class PROJECT_GGSU_API UGGSUCropsGradeSetDataAsset : public UDataAsset
 {
 	GENERATED_BODY()
 
 public:
-	UPROPERTY(EditAnywhere, BlueprintReadOnly)
-	TMap<UGGSUCropDataAsset*, float> CropsList;
+	UGGSUCropDataAsset* GetCrop(const ECropGrade CropGrade)
+	{
+		return Crops[CropGrade];
+	}
 	
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	TMap<ECropGrade, UGGSUCropDataAsset*> Crops;
 };
