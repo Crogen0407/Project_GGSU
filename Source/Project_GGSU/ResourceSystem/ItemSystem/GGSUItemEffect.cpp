bc3c62172ae621a2ae1c3a70879b2abf99594e2e
ahreum0408 <123713362+02BBQ@users.noreply.github.com>
2025-09-26 10:56:17 +0900

    fixed | 머지 에러 해결

--- Source/Project_GGSU/ResourceSystem/ItemSystem/GGSUItemEffect.cpp
+++ Source/Project_GGSU/ResourceSystem/ItemSystem/GGSUItemEffect.cpp
@@ -1,9 +1,5 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-<<<<<<<< HEAD:Source/Project_GGSU/ResourceSystem/ItemSystem/GGSUItemDataAsset.cpp
 #include "ResourceSystem/ItemSystem/GGSUItemDataAsset.h"
-========
 #include "ResourceSystem/ItemSystem/GGSUItemEffect.h"
->>>>>>>> 190ddac02da428eb998b362459ab1ba03e52c7b0:Source/Project_GGSU/ResourceSystem/ItemSystem/GGSUItemEffect.cpp
-
