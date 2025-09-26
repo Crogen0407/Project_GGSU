45e6a96876a3c15ac3e970985185522d25bff5a4
Crogen0407 <mymcoding0407@gmail.com>
2025-09-26 23:52:33 +0900

    feat : Currency default value

--- Source/Project_GGSU/ResourceSystem/GGSUCurrencyDataAsset.h
+++ Source/Project_GGSU/ResourceSystem/GGSUCurrencyDataAsset.h
@@ -13,5 +13,8 @@ UCLASS()
 class PROJECT_GGSU_API UGGSUCurrencyDataAsset : public UGGSUResourceDataAsset
 {
 	GENERATED_BODY()
-	
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	int DefaultValue;
 };
